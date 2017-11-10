c46c39138d8976428187dd67c53e434aa6cdf4ee
AlexBristow <alex.bristow@hotmail.com>
2017-11-10 17:44:35 +0000

    Player + Movement
    added player placeholder and movement on mouse hold

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float movement_speed;
+
+    // Use this for initialization
+    void Start ()
+    {
+        movement_speed = 5;		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit; 
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Vector3 destination = hit.point;
+                transform.position = Vector3.Lerp(transform.position, destination, movement_speed * Time.deltaTime);
+                Debug.Log("Destination : " + destination);
+            }
+        }
+        
+	}
+
+}
