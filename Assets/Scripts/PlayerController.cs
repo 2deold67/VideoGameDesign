c1385a303f69a10c34c823534fd2d145e2ec9961
Diogo Gomes <2deold67@solent.ac.uk>
2017-11-12 18:43:25 +0000

    Android Compatibility
    modified project for android development with Unity Remote 5 and added android movement on touch (buggy)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float movement_speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        movement_speed = 5.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Ended)
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+
+                if (Physics.Raycast(ray, out hit))
+                    if (hit.collider != null)
+                    {
+                        GoToLocation(new Vector3 (hit.point.x, transform.position.y,hit.point.z));
+                    }
+            }
+        }
+        GoToMouse();
+    }
+
+    public void GoToLocation(Vector3 location)
+    {
+
+            transform.position = Vector3.Lerp(transform.position, location, movement_speed * Time.deltaTime);
+            Debug.Log("Destination : " + location);
+    
+    }
+
+    private void GoToMouse()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Vector3 destination = hit.point;
+                transform.position = Vector3.Lerp(transform.position, destination, movement_speed * Time.deltaTime);
+                Debug.Log("Destination : " + destination);
+            }
+        }
+    }
+}
