8b352231af6af66d03f97f5740213ccb05c1689a
AlexBristow <alex.bristow@hotmail.com>
2017-11-28 15:45:35 +0000

    popup damage
    just working on the damage feeback

---
+++ Assets/Scripts/PopupDamageTextController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupDamageTextController : MonoBehaviour
+{
+    private static PopupDamageText popupText;
+
+    public static void Initialize()
+    {
+        popupText = Resources.Load<PopupDamageText>("Prefabs/PopupTextParent");
+    }
+    public static void CreatePopupDamage(string text, Transform location)
+    {
+
+        PopupDamageText instance = Instantiate(popupText);
+
+    }
+
+
+}
